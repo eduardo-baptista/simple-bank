eea10875cf360217061cd79e00c6a9489f87c827
Eduardo Baptista <eduardofbaptista@gmail.com>
2024-02-06 11:46:05 -0300

    Add withdraw use case

---
+++ internal/usecase/account/withdraw.go
@@ -0,0 +1,60 @@
+package usecase
+
+import (
+	"errors"
+	"simple-bank/internal/domain/repository"
+	"simple-bank/internal/shared/dto"
+)
+
+var (
+	ErrWithdrawFailToRetrieveAccount = errors.New("[WithdrawUseCase] Fail to retrieve account")
+	ErrWithdrawAccountNotExists      = errors.New("[WithdrawUseCase] Account not exists")
+	ErrWithdrawFailToWithdraw        = errors.New("[WithdrawUseCase] Fail to withdraw")
+	ErrWithdrawFailToUpdateAccount   = errors.New("[WithdrawUseCase] Fail to update account")
+)
+
+type WithdrawInputDTO struct {
+	Origin string
+	Amount int
+}
+
+type WithdrawOutputDTO struct {
+	Origin dto.AccountDTO
+	Amount int
+}
+
+type WithdrawUseCase struct {
+	accountRepository repository.AccountRepository
+}
+
+func NewWithdrawUseCase(accountRepository repository.AccountRepository) *WithdrawUseCase {
+	return &WithdrawUseCase{accountRepository: accountRepository}
+}
+
+func (uc *WithdrawUseCase) Execute(input WithdrawInputDTO) (*WithdrawOutputDTO, error) {
+	account, err := uc.accountRepository.GetAccountByID(input.Origin)
+	if err != nil {
+		return nil, errors.Join(ErrWithdrawFailToRetrieveAccount, err)
+	}
+
+	if account == nil {
+		return nil, ErrWithdrawAccountNotExists
+	}
+
+	err = account.Withdraw(input.Amount)
+	if err != nil {
+		return nil, errors.Join(ErrWithdrawFailToWithdraw, err)
+	}
+
+	if err = uc.accountRepository.UpdateAccount(account); err != nil {
+		return nil, errors.Join(ErrWithdrawFailToUpdateAccount, err)
+	}
+
+	return &WithdrawOutputDTO{
+		Origin: dto.AccountDTO{
+			ID:      account.ID,
+			Balance: account.Balance,
+		},
+		Amount: input.Amount,
+	}, nil
+}
---
+++ internal/usecase/account/withdraw_test.go
@@ -0,0 +1,102 @@
+package usecase
+
+import (
+	"errors"
+	"simple-bank/internal/domain/entity"
+	domainErrs "simple-bank/internal/domain/errors"
+	"simple-bank/internal/domain/repository/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"go.uber.org/mock/gomock"
+)
+
+type TestWithdrawUseCaseSuite struct {
+	suite.Suite
+	ctrl *gomock.Controller
+	repo *mocks.MockAccountRepository
+	sut  *WithdrawUseCase
+}
+
+func (suite *TestWithdrawUseCaseSuite) SetupSubTest() {
+	suite.ctrl = gomock.NewController(suite.T())
+	suite.repo = mocks.NewMockAccountRepository(suite.ctrl)
+	suite.sut = NewWithdrawUseCase(suite.repo)
+}
+
+func (suite *TestWithdrawUseCaseSuite) TearDownSubTest() {
+	suite.ctrl.Finish()
+}
+
+func (suite *TestWithdrawUseCaseSuite) TestWithdraw() {
+	suite.Run("Should withdraw amount from account", func() {
+		account := entity.NewAccount("1", 100)
+		suite.repo.EXPECT().GetAccountByID("1").Return(account, nil)
+		suite.repo.EXPECT().UpdateAccount(account).Return(nil)
+
+		output, err := suite.sut.Execute(WithdrawInputDTO{
+			Origin: "1",
+			Amount: 50,
+		})
+
+		suite.NoError(err)
+		suite.Equal(output.Origin.Balance, 50)
+		suite.Equal(output.Origin.ID, "1")
+	})
+
+	suite.Run("Should return error when fail to retrieve account", func() {
+		suite.repo.EXPECT().GetAccountByID("1").Return(nil, errors.New("[AccountRepository] internal error"))
+
+		output, err := suite.sut.Execute(WithdrawInputDTO{
+			Origin: "1",
+			Amount: 50,
+		})
+
+		suite.ErrorIs(err, ErrWithdrawFailToRetrieveAccount)
+		suite.Nil(output)
+	})
+
+	suite.Run("Should return error when withdraw account without balance", func() {
+		account := entity.NewAccount("1", 100)
+		suite.repo.EXPECT().GetAccountByID("1").Return(account, nil)
+
+		output, err := suite.sut.Execute(WithdrawInputDTO{
+			Origin: "1",
+			Amount: 150,
+		})
+
+		suite.ErrorIs(err, ErrWithdrawFailToWithdraw)
+		suite.ErrorIs(err, domainErrs.ErrAccountInsufficientBalance)
+		suite.Nil(output)
+	})
+
+	suite.Run("Should return error when fail to update account", func() {
+		account := entity.NewAccount("1", 100)
+		suite.repo.EXPECT().GetAccountByID("1").Return(account, nil)
+		suite.repo.EXPECT().UpdateAccount(account).Return(errors.New("[AccountRepository] internal error"))
+
+		output, err := suite.sut.Execute(WithdrawInputDTO{
+			Origin: "1",
+			Amount: 50,
+		})
+
+		suite.ErrorIs(err, ErrWithdrawFailToUpdateAccount)
+		suite.Nil(output)
+	})
+
+	suite.Run("Should return error when account not exists", func() {
+		suite.repo.EXPECT().GetAccountByID("1").Return(nil, nil)
+
+		output, err := suite.sut.Execute(WithdrawInputDTO{
+			Origin: "1",
+			Amount: 50,
+		})
+
+		suite.ErrorIs(err, ErrWithdrawAccountNotExists)
+		suite.Nil(output)
+	})
+}
+
+func TestWithdraw(t *testing.T) {
+	suite.Run(t, new(TestWithdrawUseCaseSuite))
+}
